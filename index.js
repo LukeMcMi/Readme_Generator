83152bae15ee308b903328088a2046054f98683b
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-10-22 20:39:48 +1100

    Added questions to index.js

--- index.js
+++ index.js
@@ -6,7 +6,66 @@ const generteReadMe = require("generateMarkdown")
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the project title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief Description of your project: ",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the project going to be used for?",
+    },
+    {
+        type: "list",
+        name: "licence",
+        message: "Choose the appropriate licence for the project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can other developers can contribute to your project?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Are any tests included?",
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What to do if there are issues?",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please provide your GitHub username: ",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please provide your email: ",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
